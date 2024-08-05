3b2ac884ee16212d39188a35c4125c22fddaa7de
farid <farid@LAPTOP-SPTVSQVM>
2024-08-05 21:37:33 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Channels;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("All arithmetic operations:");
+            int num_1 = 10;
+            int num_2 = 20;
+            Console.WriteLine(num_1+num_2); // addition
+            Console.WriteLine(num_1-num_2); //subraction
+            Console.WriteLine(num_1*num_2); //multiplication
+            Console.WriteLine((float)num_1/(float)num_2); //division
+        }
+    }
+}
